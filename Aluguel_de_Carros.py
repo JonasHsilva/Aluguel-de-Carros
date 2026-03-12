0795af1ffb713b665f10f7efec0779292c4f4bb0
Jonas Silva <jonashsilva@protonmail.com>
2026-03-12 15:42:33 -0300

    Create Aluguel_de_Carros.py

---
+++ Aluguel_de_Carros.py
@@ -0,0 +1,7 @@
+#valor da diária R$50,00
+#Valor do Km R$0,20
+
+dias = int(input('Dias Locados: ')
+km = float(input('Km rodados: ')
+valor = (dias * 50) + (km * 0.20)
+print('O total a ser pago será de R${}'.format(valor))
